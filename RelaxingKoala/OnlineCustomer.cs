64950c6d0a27d39e493f9abd42b09201cf9b8207
Ned Madigan <nedmadigan4@gmail.com>
2024-05-10 12:12:55 +1000

    setup basics for OnlineCustomer, Table, MenuItem

---
+++ RelaxingKoala/OnlineCustomer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelaxingKoala
+{
+    internal class OnlineCustomer
+    {
+        public int ID { get; private set; }
+        public string FullName {  get; private set; }
+        public string PhoneNumber {  get; private set; }
+        public string Email {  get; private set; }
+        public int TableReservedID { get; private set; }
+
+        public OnlineCustomer(int aId, string aFullName, string aPhoneNumber, string aEmail)
+        {
+            ID = aId;
+            FullName = aFullName;
+            PhoneNumber = aPhoneNumber;
+            Email = aEmail;
+        }
+    }
+}
---
+++ RelaxingKoala/Table.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelaxingKoala
+{
+    internal class Table
+    {
+        private int _id;
+        private OnlineCustomer? _reserver;
+        public int ID { get { return _id; } }   
+        public bool IsAvailable { get { return _reserver != null; } }
+
+        public Table(int aId, OnlineCustomer? reserver = null)
+        {
+            _id = aId;
+            _reserver = reserver;
+        }
+
+        public void reserveTable(OnlineCustomer reserver)
+        {
+            _reserver = reserver;
+            // Do something else?
+        }
+    }
+}
